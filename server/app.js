65020bccbb3e14660ac9da795fcd2b8daf3abd77
vasyaboychuk <boychuk98vasya@gmail.com>
2023-02-23 15:27:42 +0200

    done

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 require('dotenv').config();
 const sequelize = require('./dataBase/db');
-const config = require('./configs/config');
 const models = require('./dataBase/index');
 
 const osRouter = require('./router/os.router');
@@ -11,26 +10,28 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const PORT=process.env.PORT||5000;
+
 app.use('/api/v1/health', osRouter);
 app.use('/api/v1/auth', userRouter);
 
 app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({
-        message: err.message || 'unknown error',
-        status: err.status || 500,
+  res.status(err.status || 500).json({
+    message: err.message || 'unknown error',
+    status: err.status || 500,
 
-    });
+  });
 });
 
 const start = async () => {
-    try {
-        await sequelize.authenticate();
-        await sequelize.sync();
-        app.listen(config.PORT, () => {
-            console.log(`server listen port: ${config.PORT}`);
-        });
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => {
+      console.log(`server listen port: ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
 start();
--- server/dataBase/User.js
+++ server/dataBase/User.js
@@ -2,29 +2,29 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('./db');
 
 const User = sequelize.define('user', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: {
-        type: DataTypes.STRING,
-        trim: true,
-        validate: {
-            len: [3, 50],
-        },
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: {
+    type: DataTypes.STRING,
+    trim: true,
+    validate: {
+      len: [3, 50],
     },
-    email: {
-        type: DataTypes.STRING,
-        trim: true,
-        unique: true,
-        validate: {
-            isEmail: true,
-        },
+  },
+  email: {
+    type: DataTypes.STRING,
+    trim: true,
+    unique: true,
+    validate: {
+      isEmail: true,
     },
-    password: { type: DataTypes.STRING },
+  },
+  password: { type: DataTypes.STRING },
 
 }, {
-    timestamps: true,
-    createdAt: true,
-    updatedAt: true,
+  timestamps: true,
+  createdAt: true,
+  updatedAt: true,
 });
 module.exports = {
-    User,
+  User,
 };
